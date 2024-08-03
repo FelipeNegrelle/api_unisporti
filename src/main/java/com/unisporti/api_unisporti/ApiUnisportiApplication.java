dfd1be49afc65bd19233387e6fe40ad81cf01c7c
Felipe Ramos <felipenegrelleramos@gmail.com>
2024-08-03 16:57:55 -0300

    Initital commit

---
+++ src/main/java/com/unisporti/api_unisporti/ApiUnisportiApplication.java
@@ -0,0 +1,13 @@
+package com.unisporti.api_unisporti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUnisportiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUnisportiApplication.class, args);
+	}
+
+}
